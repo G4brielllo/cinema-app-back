c10875530f9689e0c44f33cd771f79086849b2cf
G4brielllo <gabrieltarasiuk3@gmail.com>
2025-04-07 22:05:31 +0200

    commit init

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/api/register', [AuthController::class, 'register']);
+
+Route::post('/api/login', [AuthController::class, 'login']);
+
+Route::post('/api/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/api/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+
